205e0532262b25f468e1f296dbc4192289f0c194
Jack Sloan <jbsloan10@gmail.com>
2021-04-16 23:30:54 -0500

    add placholder pages for articles and projects

---
+++ src/routes/article.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>Jack Sloan | Articles</title>
+</svelte:head>
+
+<section class="prose">
+	<h2 class="text-xl">Articles</h2>
+	<p>
+		<span class="mr-2">&#128679;</span>Under construction... &#128679;
+	</p>
+</section>
---
+++ src/routes/project.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Jack Sloan | Projects</title>
+</svelte:head>
+
+<section class="prose">
+	<h2 class="text-xl">Projects</h2>
+	<span class="mr-2">&#128679;</span>Under construction... &#128679;
+</section>
