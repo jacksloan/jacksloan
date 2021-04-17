de37a77e75d11564d418550170e7fe4a27fdff94
Jack Sloan <jbsloan10@gmail.com>
2021-04-16 23:29:21 -0500

    add nav and window to every page

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,5 +1,14 @@
 <script>
 	import '../app.postcss';
+	import Nav from '$lib/Nav.svelte';
+	import Window from '$lib/Window.svelte';
 </script>
 
-<slot />
+<div class="container mx-auto h-screen flex flex-col items-center p-8">
+	<Nav />
+	<Window containerClass="mt-8" toolbarText="Jack Sloan - Software Developer">
+		<main class="p-6">
+			<slot />
+		</main>
+	</Window>
+</div>
