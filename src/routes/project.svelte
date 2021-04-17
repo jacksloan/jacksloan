c8b5e0e4affd089e7792d16ded242c9eecdf7a61
Jack Sloan <jbsloan10@gmail.com>
2021-04-17 17:27:01 -0500

    a github links to project page

--- src/routes/project.svelte
+++ src/routes/project.svelte
@@ -1,3 +1,22 @@
+<script context="module" lang="ts">
+	export async function load({ fetch }) {
+		const response = await fetch('https://api.github.com/users/jacksloan/repos');
+		const repos = await response.json();
+		return {
+			props: {
+				repos
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	export let repos: any[];
+	$: activeRepos = repos.filter((r) => {
+		return r.archived === false && r.private === false && r.fork === false;
+	});
+</script>
+
 <svelte:head>
 	<title>Jack Sloan | Projects</title>
 </svelte:head>
@@ -5,4 +24,14 @@
 <section class="prose">
 	<h2 class="text-xl">Projects</h2>
 	<span class="mr-2">&#128679;</span>Under construction... &#128679;
+
+	<ul>
+		{#each activeRepos as r}
+			<li>
+				<a href={r.html_url} target="_blank">
+					{r.description || r.name}
+				</a>
+			</li>
+		{/each}
+	</ul>
 </section>
