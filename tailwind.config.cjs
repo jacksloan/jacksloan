8f624da88a601fce1235697be11873868b60a1fb
jacksloan <jbsloan10@gmail.com>
2022-03-13 12:18:26 -0500

    firebase - move svelte pages to temp folder

--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	mode: 'jit',
-	darkMode: 'class',
-	purge: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {}
-	},
-	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')]
-};
